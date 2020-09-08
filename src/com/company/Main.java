63e2acbf960928616f6ed9c09e71414f0070e679
Maxim Kirin <maxkire111@gmail.com>
2020-09-08 23:19:18 +0300

    second commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Ball ball1 = new Ball(150,15);
+	    System.out.println(ball1);
+	    ball1.move(115, 150);
+	    System.out.println(ball1);
+
+    }
+
+
+}
